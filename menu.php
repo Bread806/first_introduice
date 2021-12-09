4df9985499c4e4ae954546c9a96d19dbf3532d64
Bread806 <bsw0210903@gmail.com>
2021-12-09 20:58:17 +0800

    0858

--- menu.php
+++ menu.php
@@ -53,7 +53,7 @@
     </div>
 	
 	
-<?php
+<!--<?php
 $con = @mysqli_connect(
 	'us-cdbr-east-04.cleardb.com',
 	'b33535106a6db3',
@@ -68,7 +68,24 @@ else{
 	echo "Hi! I am 45P!";
 }
 mysqli_close($con);
-?> 
+?> -->
+
+<?php
+// 建立MySQL的資料庫連接 
+$link = @mysqli_connect( 
+            'localhost',  // MySQL主機名稱 
+            'root',       // 使用者名稱 
+            '1234',  // 密碼 
+            'test');  // 預設使用的資料庫名稱 
+if ( !$link ) {
+   echo "MySQL資料庫連接錯誤!<br/>";
+   exit();
+}
+else {
+   echo "MySQL資料庫test連接成功!<br/>";
+}
+mysqli_close($link);  // 關閉資料庫連接
+?>
 </body>
 
 </html>
